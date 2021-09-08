d22e7bf0d6cffdd35e2335327db00e759da67edd
faza <fazaasulaiman@gmail.com>
2021-09-08 13:08:23 +0800

    fixing usename profile

--- app/Views/template/_template.php
+++ app/Views/template/_template.php
@@ -135,11 +135,9 @@
             <ul class=" navbar-right">
               <li class="nav-item dropdown open" style="padding-left: 15px;">
                 <a href="javascript:;" class="user-profile dropdown-toggle" aria-haspopup="true" id="navbarDropdown" data-toggle="dropdown" aria-expanded="false">
-                  <img src="<?php echo base_url(); ?>/assets/production/images/img.jpg" alt="">John Doe
+                  <img src="<?php echo base_url(); ?>/assets/production/images/img.jpg" alt=""><?php echo session('username'); ?>
                 </a>
                 <div class="dropdown-menu dropdown-usermenu pull-right" aria-labelledby="navbarDropdown">
-                  <a class="dropdown-item" href="javascript:;"> Profile</a>
-                  <a class="dropdown-item" href="javascript:;">Help</a>
                   <a class="dropdown-item" href="<?php echo base_url(); ?>/logout"><i class="fa fa-sign-out pull-right"></i> Log Out</a>
                 </div>
               </li>
